1cbd6ab277cedbd9b05ce8245b4a307a23fa10a8
Anemaygi <gianemayumisan@gmail.com>
2024-10-30 10:03:37 -0300

    refactor: remove unused code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,6 @@ import palavrasComuns from '@/settings/palavras-comuns.json'
 
 export default function Home() {
 
-  const settingKeyClassname = "rounded-3xl border w-full h-full flex items-center justify-center drop-shadow-md"
-
   return (
     <div className="w-full h-full grid grid-cols-5 gap-4 [&>*]:rounded-3xl [&>*]:border [&>*]:w-full [&>*]:h-full [&>*]:flex [&>*]:items-center [&>*]:justify-center [&>*]:drop-shadow-md">
       {palavrasComuns.keys.map((key, index) => (
