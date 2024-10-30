81b65dc9c296e05982e207f5777f65f1d96a189f
Anemaygi <gianemayumisan@gmail.com>
2024-10-30 09:09:48 -0300

    style: styling keys from palavras comuns

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   const boxes = Array.from({ length: 7*3-6 }, (_, index) => `Caixa ${index + 1}`);
 
   return (
-    <div className="w-min-screen h-screen grid grid-cols-7 gap-4 p-4 [&>*]:rounded-3xl [&>*]:border [&>*]:w-full [&>*]:h-full [&>*]:flex [&>*]:items-center [&>*]:justify-center [&>*]:drop-shadow-md">
+    <div className="w-min-screen text-xl h-screen grid grid-cols-7 gap-4 p-4 [&>*]:rounded-3xl [&>*]:border [&>*]:w-full [&>*]:h-full [&>*]:flex [&>*]:items-center [&>*]:justify-center [&>*]:drop-shadow-md">
       
       <div key={0} className="" style={{ backgroundColor:"#737376", borderColor: "#0E0B32", color:"white" }}>FALAR</div>
       <div key={1} className="col-span-5 bg-white border-black">
@@ -12,15 +12,15 @@ export default function Home() {
       </div>
       <div key={2} className="" style={{ backgroundColor:"#737376", borderColor: "#0E0B32", color:"white" }}>APAGAR ÚLTIMO</div>
 
-      <div key={3} className="" style={{ backgroundColor:"#737376", borderColor: "#0E0B32", color:"white" }}>CONF 1</div>
-      <div key={4} className="" style={{ backgroundColor:"#737376", borderColor: "#0E0B32", color:"white" }}>CONF 2</div>
-      <div key={5} className="" style={{ backgroundColor:"#737376", borderColor: "#0E0B32", color:"white" }}>CONF 3</div>
-      <div key={6} className="" style={{ backgroundColor:"#737376", borderColor: "#0E0B32", color:"white" }}>CONF 4</div>
-      <div key={7} className="" style={{ backgroundColor:"#737376", borderColor: "#0E0B32", color:"white" }}>CONF 5</div>
-      <div key={8} className="" style={{ backgroundColor:"#737376", borderColor: "#0E0B32", color:"white" }}>CONF 6</div>
+      <div key={3} className="" style={{ backgroundColor:"#737376", borderColor: "#0E0B32", color:"white" }}>APAGAR TUDO</div>
+      <div key={4} className="" style={{ backgroundColor:"#737376", borderColor: "#0E0B32", color:"white" }}>VOLTAR</div>
+      <div key={5} className="" style={{ backgroundColor:"#737376", borderColor: "#0E0B32", color:"white" }}>CONFIGURAÇÕES</div>
+      <div key={6} className="" style={{ backgroundColor:"#737376", borderColor: "#0E0B32", color:"white" }}>PALAVRAS COMUNS</div>
+      <div key={7} className="" style={{ backgroundColor:"#737376", borderColor: "#0E0B32", color:"white" }}>GRUPOS DE PALAVRAS</div>
+      <div key={8} className="" style={{ backgroundColor:"#737376", borderColor: "#0E0B32", color:"white" }}>LETRAS</div>
 
       {palavrasComuns.keys.map((key, index) => (
-        <div key={index+8} className="" style={{ backgroundColor:key.backgroundColor, borderColor: key.borderColor, color:key.textColor }}>
+        <div key={index+8} className="font-bold uppercase" style={{ backgroundColor:key.backgroundColor, borderColor: key.borderColor, color:key.textColor }}>
           {key.value}
         </div>
       ))}
